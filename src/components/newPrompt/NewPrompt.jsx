831a641f0154608a9dd11f8ed3802d4c5139176d
ArunaluB <IT22515612@my.sliit.lk>
2024-10-02 00:09:43 +0530

    gemini set

--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -1,28 +1,53 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import "./newPrompt.css"
-import { useEffect , useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { IKImage } from "imagekitio-react";
+import Upload from '../upload/Upload';
+import model from "../../lib/gemini";
 const NewPrompt = () => {
-    const endRef = useRef(null);
-  
-  useEffect (() => {
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+    aiData: {},
+  });
+  const endRef = useRef(null);
+
+  useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
   }, []);
-    return (
-        <>
-        <div className="endChat" ref={endRef}></div>
-        <form className="newForm" >
-          <label htmlFor="file">
-            <img src="/attachment.png" alt="" />
-          </label>
-          <input id="file" type="file" multiple={false} hidden />
-          <input type="text" name="text" placeholder="Ask anything..." />
-          <button>
-            <img src="/arrow.png" alt="" />
-          </button>
-        </form>
-      </>
-    )
+ 
+  const add = async () => {
+    const prompt = "write a react js";
+    const result = model.generateContent(prompt);
+    const response = (await result).response;
+    const text = response.text();
+    console.log(text);
+  };
+  return (
+    <>
+      {/* ADD NEW CHAT */}
+      {img.isLoading && <div className="">Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ width: 380 }]}
+        />
+      )}
+      <div className="endChat" ref={endRef}></div>
+      <form className="newForm" >
+        <Upload setImg={setImg} />
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" name="text" placeholder="Ask anything..." />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
+    </>
+  )
 }
 
 export default NewPrompt
